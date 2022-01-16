20b3e74913791681aa90e1b18ad15982478f6280
Ibrahim <ialakash@live.com>
2022-01-16 15:42:01 -0600

    database

--- planner_database.sql
+++ planner_database.sql
@@ -37,7 +37,7 @@ CREATE TABLE `assignments` (
   `grade` float DEFAULT NULL,
   PRIMARY KEY (`assignment_id`),
   UNIQUE KEY `idassignments_UNIQUE` (`assignment_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -46,7 +46,7 @@ CREATE TABLE `assignments` (
 
 LOCK TABLES `assignments` WRITE;
 /*!40000 ALTER TABLE `assignments` DISABLE KEYS */;
-INSERT INTO `assignments` VALUES (7,12,'incomplete','2022-01-20 05:59:00','problemset','Vectors, dot products, and projections practice','Homework 1','https://openstax.org/details/books/calculus-volume-3',NULL,NULL),(8,12,'complete','2022-01-14 05:59:00','problemset','In-Class assignment to learn about vectors, projections, orthogonal projections, and dot products','Dot Product Worksheet','https://openstax.org/books/calculus-volume-3/pages/2-3-the-dot-product',NULL,NULL),(9,22,'incomplete','2022-01-18 18:00:00','essays','FWIS Students ONLY - Narrative essay on engineering','Pre-SM Essay','https://canvas.rice.edu/courses/45708/files/3329726/download?download_frd=1',NULL,NULL),(10,22,'incomplete','2022-01-17 05:59:00','problemset','Quiz','Engineering Design Process','https://canvas.rice.edu/courses/45708/quizzes/62304?module_item_id=435591',NULL,NULL),(11,22,'incomplete','2022-01-17 05:59:00','problemset','Quiz','Clarifying Team Assignment','https://canvas.rice.edu/courses/45708/quizzes/62295?module_item_id=435593',NULL,NULL),(12,22,'incomplete','2022-01-17 05:59:00','problemset','Edit the ePortfolio homepage','Edit Digication Homepage','https://rice.digication.com/ibrahim-al-akash/home-1',NULL,NULL),(13,21,'incomplete','2022-01-20 05:59:00','problemset','Hello Goodbye, If Else Statements, and Drivers','Quiz 0.2','https://canvas.rice.edu/courses/45735/assignments',NULL,NULL),(14,21,'complete','2022-01-15 05:59:00','problemset','Let\'s Start','Quiz 0.1','https://canvas.rice.edu/courses/45735/assignments',NULL,NULL);
+INSERT INTO `assignments` VALUES (7,12,'complete','2022-01-20 05:59:00','problemset','Vectors, dot products, and projections practice','Homework 1','https://openstax.org/details/books/calculus-volume-3',NULL,NULL),(8,12,'complete','2022-01-14 05:59:00','problemset','In-Class assignment to learn about vectors, projections, orthogonal projections, and dot products','Dot Product Worksheet','https://openstax.org/books/calculus-volume-3/pages/2-3-the-dot-product',1,1),(9,22,'incomplete','2022-01-18 18:00:00','essays','FWIS Students ONLY - Narrative essay on engineering','Pre-SM Essay','https://canvas.rice.edu/courses/45708/files/3329726/download?download_frd=1',NULL,NULL),(10,22,'complete','2022-01-17 05:59:00','problemset','Quiz','Engineering Design Process','https://canvas.rice.edu/courses/45708/quizzes/62304?module_item_id=435591',NULL,NULL),(11,22,'complete','2022-01-17 05:59:00','problemset','Quiz','Clarifying Team Assignment','https://canvas.rice.edu/courses/45708/quizzes/62295?module_item_id=435593',NULL,NULL),(12,22,'complete','2022-01-17 05:59:00','problemset','Edit the ePortfolio homepage','Edit Digication Homepage','https://rice.digication.com/ibrahim-al-akash/home-1',NULL,NULL),(13,21,'complete','2022-01-20 05:59:00','problemset','Hello Goodbye, If Else Statements, and Drivers','Quiz 0.2','https://canvas.rice.edu/courses/45735/assignments',NULL,NULL),(14,21,'complete','2022-01-15 05:59:00','problemset','Let\'s Start','Quiz 0.1','https://canvas.rice.edu/courses/45735/assignments',10,10),(15,19,'complete','2022-01-19 15:00:00','problemset','Pressure and Gas Properties','HW 01','https://openstax.org/books/chemistry-2e/pages/9-2-relating-pressure-volume-amount-and-temperature-the-ideal-gas-law',NULL,NULL),(16,21,'complete','2022-01-20 05:59:00','problemset','For loops practice','Quiz 0.3','https://canvas.rice.edu/courses/45735/assignments/237868',NULL,NULL),(17,21,'complete','2022-01-11 18:59:00','problemset','Beginning of Semester Survey','Quiz 0.0','https://canvas.rice.edu/courses/45735/assignments/237868',10,10),(18,12,'complete','2022-01-11 18:59:00','other','Attendance','Attendance','https://canvas.rice.edu/courses/45735/assignments/237868',5,5),(19,21,'complete','2022-01-15 05:59:00','other','Attendance','RLA Participation Week 1','https://canvas.rice.edu/courses/45735/assignments',10,10),(20,12,'complete','2022-01-14 05:59:00','problemset','Cross Product in-class group assignment','Cross Product Worksheet','https://openstax.org/details/books/calculus-volume-3',1,1),(21,23,'incomplete','2022-01-18 05:59:00','problemset','\"Electronic and Magnetic Anisotropies in FeSe Family of Iron-Based Superconductors\"','Homework 1','https://canvas.rice.edu/courses/47668/files/3357635?wrap=1',NULL,NULL);
 /*!40000 ALTER TABLE `assignments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -62,6 +62,7 @@ CREATE TABLE `classes` (
   `name` varchar(255) NOT NULL,
   `syllabus` varchar(255) DEFAULT NULL,
   `professor` varchar(255) DEFAULT NULL,
+  `semester` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`class_id`),
   UNIQUE KEY `idnew_table_UNIQUE` (`class_id`),
   UNIQUE KEY `name_UNIQUE` (`name`)
@@ -74,8 +75,40 @@ CREATE TABLE `classes` (
 
 LOCK TABLES `classes` WRITE;
 /*!40000 ALTER TABLE `classes` DISABLE KEYS */;
-INSERT INTO `classes` VALUES (12,'Multivariable Calculus','MATH212_syllabus.pdf','Shawn Williams'),(18,'Biology','BIOS201_syllabus.pdf','Dr. Collin Thomas'),(19,'Chemistry','CHEM122_syllabus.pdf','Dr. Lesa Tran Lu'),(20,'Chemistry Lab','CHEM124_syllabus.pdf','Dr. Humayun Kabir'),(21,'Engineering Computation','CAAM210_syllabus.pdf','Dr. Anastasiya Protasov'),(22,'Engineering Design','ENGI120_syllabus.pdf','Dr. Deirdre Hunter'),(23,'Astronomy','PHYS116_syllabus.pdf','Eduardo Ibarra Garcia Padilla'),(24,'Physics Computer','PHYS100_syllabus.pdf','Dr. Robert Beaird');
+INSERT INTO `classes` VALUES (12,'Multivariable Calculus','MATH212_syllabus.pdf','Shawn Williams','S22'),(18,'Biology','BIOS201_syllabus.pdf','Dr. Collin Thomas','S22'),(19,'Chemistry','CHEM122_syllabus.pdf','Dr. Lesa Tran Lu','S22'),(20,'Chemistry Lab','CHEM124_syllabus.pdf','Dr. Humayun Kabir','S22'),(21,'Engineering Computation','CAAM210_syllabus.pdf','Dr. Anastasiya Protasov','S22'),(22,'Engineering Design','ENGI120_syllabus.pdf','Dr. Deirdre Hunter','S22'),(23,'Astronomy','PHYS116_syllabus.pdf','Eduardo Ibarra Garcia Padilla','S22'),(24,'Physics Computer','PHYS100_syllabus.pdf','Dr. Robert Beaird','S22');
 /*!40000 ALTER TABLE `classes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `coursedb`
+--
+
+DROP TABLE IF EXISTS `coursedb`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `coursedb` (
+  `coursedb_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `link` varchar(255) DEFAULT NULL,
+  `description` longtext,
+  `code` varchar(255) NOT NULL,
+  `hours` int NOT NULL,
+  `level` int NOT NULL,
+  `distribution` int DEFAULT NULL,
+  `requirement` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`coursedb_id`),
+  UNIQUE KEY `idnew_table_UNIQUE` (`coursedb_id`),
+  UNIQUE KEY `code_UNIQUE` (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `coursedb`
+--
+
+LOCK TABLES `coursedb` WRITE;
+/*!40000 ALTER TABLE `coursedb` DISABLE KEYS */;
+/*!40000 ALTER TABLE `coursedb` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -91,9 +124,10 @@ CREATE TABLE `gpa` (
   `letter` varchar(10) DEFAULT NULL,
   `hours` int DEFAULT NULL,
   `quality_points` float DEFAULT NULL,
+  `semester` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`gpa_id`),
   UNIQUE KEY `idgpa_UNIQUE` (`gpa_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -102,7 +136,7 @@ CREATE TABLE `gpa` (
 
 LOCK TABLES `gpa` WRITE;
 /*!40000 ALTER TABLE `gpa` DISABLE KEYS */;
-INSERT INTO `gpa` VALUES (5,'Multivariable Calculus',NULL,3,NULL),(6,'Chemistry',NULL,3,NULL),(7,'Chemistry Lab',NULL,1,NULL),(8,'Biology',NULL,3,NULL),(9,'Engineering Computation',NULL,3,NULL),(10,'Engineering Design',NULL,3,NULL),(11,'Astronomy',NULL,1,NULL),(12,'Physics Computer',NULL,1,NULL),(13,'hk',NULL,NULL,NULL);
+INSERT INTO `gpa` VALUES (-4,'Intro to Psych','A+',3,12,'F21'),(-2,'Mechanics','B+',3,13.3332,'F21'),(-1,'ODE + Linear Algebra','B+',4,9.9999,'F21'),(5,'Multivariable Calculus',NULL,3,NULL,'S22'),(6,'Chemistry II',NULL,3,NULL,'S22'),(7,'Chemistry II Lab',NULL,1,NULL,'S22'),(8,'Biology',NULL,3,NULL,'S22'),(9,'Engineering Computation',NULL,3,NULL,'S22'),(10,'Engineering Design',NULL,3,NULL,'S22'),(11,'Astronomy',NULL,1,NULL,'S22'),(12,'Physics Computer',NULL,1,NULL,'S22');
 /*!40000 ALTER TABLE `gpa` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -116,14 +150,14 @@ DROP TABLE IF EXISTS `graded_assignments`;
 CREATE TABLE `graded_assignments` (
   `graded_id` int NOT NULL AUTO_INCREMENT,
   `assignment_id` int NOT NULL,
-  `grading_id` int NOT NULL,
   `grade` float NOT NULL,
   `maximum` float DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `grading_id` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`graded_id`),
   UNIQUE KEY `graded_id_UNIQUE` (`graded_id`),
-  UNIQUE KEY `assignment_id_UNIQUE` (`assignment_id`),
-  UNIQUE KEY `grading_id_UNIQUE` (`grading_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `assignment_id_UNIQUE` (`assignment_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -132,6 +166,7 @@ CREATE TABLE `graded_assignments` (
 
 LOCK TABLES `graded_assignments` WRITE;
 /*!40000 ALTER TABLE `graded_assignments` DISABLE KEYS */;
+INSERT INTO `graded_assignments` VALUES (3,18,5,5,'Attendance','1'),(4,17,10,10,'Quizzes','8'),(6,14,10,10,'','8'),(7,19,10,10,'','11'),(8,8,1,1,'','2'),(9,20,1,1,'','2');
 /*!40000 ALTER TABLE `graded_assignments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -151,7 +186,7 @@ CREATE TABLE `gradingscheme` (
   `item_count` int DEFAULT NULL,
   PRIMARY KEY (`grading_id`),
   UNIQUE KEY `idgradingscheme_UNIQUE` (`grading_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -160,6 +195,7 @@ CREATE TABLE `gradingscheme` (
 
 LOCK TABLES `gradingscheme` WRITE;
 /*!40000 ALTER TABLE `gradingscheme` DISABLE KEYS */;
+INSERT INTO `gradingscheme` VALUES (1,12,'Attendance',0,5,NULL),(2,12,'Homework - Online',0,5,NULL),(3,12,'Homework - Written',0,20,NULL),(4,12,'Midterm 1',0,20,NULL),(5,12,'Midterm 2',0,20,NULL),(6,12,'Final',0,30,NULL),(7,21,'Unpledged Projects',0,50,NULL),(8,21,'Quizzes',0,15,NULL),(9,21,'Pledged Projects',0,20,NULL),(10,21,'Group Assignments',0,5,NULL),(11,21,'RLA Attendance',0,10,NULL),(12,21,'Optional Project',0,1,NULL);
 /*!40000 ALTER TABLE `gradingscheme` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -180,7 +216,7 @@ CREATE TABLE `readings` (
   `status` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`readings_id`),
   UNIQUE KEY `idreadings_UNIQUE` (`readings_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -189,7 +225,7 @@ CREATE TABLE `readings` (
 
 LOCK TABLES `readings` WRITE;
 /*!40000 ALTER TABLE `readings` DISABLE KEYS */;
-INSERT INTO `readings` VALUES (5,12,'https://canvas.rice.edu/courses/46724/external_tools/3912','2022-01-14','Videos 3.1-3.2','Cross Product','complete'),(6,19,'https://openstax.org/books/chemistry-2e/pages/9-3-stoichiometry-of-gaseous-substances-mixtures-and-reactions','2022-01-14','Chapter 9.3','Stoichiometry of Gaseous Substances, Mixtures, and Reactions',NULL);
+INSERT INTO `readings` VALUES (5,12,'https://canvas.rice.edu/courses/46724/external_tools/3912','2022-01-14','Videos 3.1-3.2','Cross Product','complete'),(6,19,'https://openstax.org/books/chemistry-2e/pages/9-3-stoichiometry-of-gaseous-substances-mixtures-and-reactions','2022-01-14','Chapter 9.3','Stoichiometry of Gaseous Substances, Mixtures, and Reactions','complete'),(7,19,'https://cnx.org/contents/xq3piGJg@2.1:aPpvPV9J@2/The-Kinetic-Molecular-Theory','2022-01-19','CDN 15','The Kinetic Molecular Theory','incomplete'),(8,19,'https://openstax.org/books/chemistry-2e/pages/9-6-non-ideal-gas-behavior','2022-01-19','Chapter 9.6','Non-Ideal Gas Behavior','incomplete'),(9,18,'https://canvas.rice.edu/courses/45504/files/3352762/download?wrap=1','2022-01-19','Miller Paper','A Production of Amino Acids Under Primitive Earth Conditions','incomplete'),(10,18,'https://canvas.rice.edu/courses/45504/assignments/syllabus','2022-01-19','Chapter 2','The Chemical Context of Life','incomplete');
 /*!40000 ALTER TABLE `readings` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -202,4 +238,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-14 10:07:07
+-- Dump completed on 2022-01-16 15:41:42
